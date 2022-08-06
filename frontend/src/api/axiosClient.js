1810271dd7e2a215fe3c2ad3f6a7117f6d5556c4
Khoa Duong <khoaduong1398@gmail.com>
2022-08-06 23:45:38 +0700

    change url api

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'http://localhost:8080/nihongo/admin/api',
+    baseURL: 'https://api-nihongo.herokuapp.com/nihongo/admin/api',
     headers: {
         'Content-Type': 'application/json',
     },
